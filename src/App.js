7e7c87bef9d00759d91055f571f21ee4e3632b93
Pranshu <135404533+PranshuRawat@users.noreply.github.com>
2023-09-03 15:37:28 +0000

    branch added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import "slick-carousel/slick/slick.css"; 
+// import logo from './logo.svg';
+import "./App.css";
+
+// Routing
+import { Routes, Route } from "react-router-dom";
+
+// React Slick CSS
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './App.css';
+
+// Pages
+import PlayPage from "./pages/Play.Page";
+import HomePage from "./pages/Home.Page";
+import MoviePage from "./pages/Movie.Page";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  return <Routes>
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/movie/:id" element={<MoviePage/>}/>
+    <Route path="/plays" element={<PlayPage/>}/>
+  </Routes>;
 }
 
 export default App;
